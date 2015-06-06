bf3afb75945ebc7fd2f6048426eed5cfec6d9d84
Ruhul Amin <ramin.2co@gmail.com>
2015-06-06 14:07:53 -0400

    completed cucumber basics

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,23 @@
 #Create your feature here
+Feature: animal
+  In order to properly verify an animal I like to know its name,type,age and if it is old or not.
+
+  Scenario: name
+    Given a animal with name tom
+    When I retreive the name value
+    Then It should be tom
+
+  Scenario:  age
+    Given a animal have age 3 years
+    When I retreive the age value
+    Then It should be 3
+
+  Scenario:  type
+    Given a animal type being a dog
+    When I retreive the type value
+    Then It should be dog
+
+  Scenario:  old
+    Given a animal is old
+    When I retreive the old value
+    Then It should be true
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,42 @@
 #Delete this comment, here is where you should write your step defs
+Given(/^a animal with name (.*)$/) do |name|
+  @animal=Animal.new(name,'dog',3)
+
+end
+
+When(/^I retreive the name value$/) do
+  @value=@animal.name
+end
+
+
+Then(/^It should be (.*)$/) do |value|
+  expect(@value).to eq value
+end
+
+Given(/^a animal have age (\d+) years$/) do |age|
+  @animal=Animal.new('name','dog',age)
+end
+When(/^I retreive the age value$/) do
+  @value=@animal.age
+end
+
+Given(/^a animal type being a (.*)$/) do |type|
+  @animal=Animal.new('name',type,3)
+end
+
+
+When(/^I retreive the type value$/) do
+  @value=@animal.type
+end
+
+Given(/^a animal is old$/) do
+  old_age = 4
+  @animal=Animal.new('name','type',old_age)
+end
+
+
+When(/^I retreive the old value$/) do
+  @value=@animal.old?.to_s
+end
+
+
